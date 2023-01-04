f092912de0ca2db16840ef80cc9be3f7f39509c1
Faris Abbasi <mfarisabbasi@gmail.com>
2023-01-05 01:43:09 +0500

    added vehicleModel

---
+++ backend/models/vehicleModel.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const vehicleSchema = new mongoose.Schema({
+  make: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  mileage: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: [String],
+    required: false,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  engine: {
+    type: String,
+    required: true,
+  },
+  airbags: {
+    type: Number,
+    required: true,
+  },
+  transmission: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Vehicle = mongoose.model("Vehicle", vehicleSchema);
+
+export default Vehicle;
