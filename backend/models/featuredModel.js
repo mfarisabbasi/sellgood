49caf51399aa04b80fc25cecccac6789cf2aa143
Faris Abbasi <mfarisabbasi@gmail.com>
2023-01-05 02:09:37 +0500

    added featuredModel

---
+++ backend/models/featuredModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const featuredSchema = new mongoose.Schema({
+  adId: {
+    type: String,
+    required: true,
+  },
+  featuredUntil: {
+    type: Date,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Featured = mongoose.model("Featured", featuredSchema);
+
+export default Featured;
